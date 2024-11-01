d14e2fe58f122f9dc3f34680c200baf279c8358c
Liux <2213746416@qq.com>
2024-11-02 01:08:39 +0800

    第二次提交，修改中文显示乱码

---
+++ text5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// 九九乘法表
+int main()
+{
+    int i , j;
+    for ( i = 1; i <= 9; i++)
+    {
+        for ( j = 1 ; j <= i; j++)
+        {
+            printf("%d*%d=%d",j,i,i*j);
+            if(i * j < 10){
+                printf("   ");
+            }else{
+                printf("  ");
+            }
+        }
+       printf("\n");
+    }
+    return 0;
+}
